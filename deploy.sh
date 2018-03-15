1540a52397732937f03da2ad70e0b2106db865ff
suresh <suresh@gmail.com>
2018-03-14 23:28:42 -0700

    deploy file added

---
+++ deploy.sh
@@ -0,0 +1,2 @@
+sshpass -p "root" scp ${WORKSPACE}/target/CounterWebApp.war root@172.17.0.2:/home/practice/apache-tomcat-8.5.28/webapps
+sshpass -p "root" ssh root@172.17.0.2 "JAVA_HOME=/home/practice/jdk1.8.0_161" "/home/practice/apache-tomcat-8.5.28/bin/startup.sh"
